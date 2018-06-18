1b2f342e67333ea0dc7f080e2df8fde376099e65
yuanzhoucq <yuanzhou.cq@gmail.com>
2018-06-18 17:39:14 +0200

    fix timezone for weather

--- middleware/Weather.js
+++ middleware/Weather.js
@@ -1,11 +1,12 @@
 const rp = require('request-promise');
+const moment = require('moment-timezone');
 
 async function getWeather() {
   const w =  await rp({
     uri: 'https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="paris, france") and u = "c"&format=json',
     json: true
   });
-  const hour = new Date().getHours();
+  const hour = moment().tz("Europe/Paris").hour();
   const day = hour <= 18 ? 'Today':'Tomorrow';
   const dayIndex = hour <= 18 ? 0:1;
   const {date, text, high, low} = w.query.results.channel.item.forecast[dayIndex];
